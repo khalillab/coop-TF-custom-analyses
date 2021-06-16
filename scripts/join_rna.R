afb163e9a58167d784019d71f365c8c94edb8bbe
James Chuang <chuang.ja@gmail.com>
2021-06-16 17:11:53 -0400

    Add RNA-seq fdr to chip/rna tsv.

--- scripts/join_rna.R
+++ scripts/join_rna.R
@@ -12,10 +12,10 @@ import = function(path){
 }
 
 import(high) %>%
-    left_join(import(low) %>% select(-rna_log10_padj_high_v_reporter),
+    left_join(import(low) %>% rename(rna_log10_padj_low_v_reporter=rna_log10_padj_high_v_reporter),
               by=c("chrom", "start", "end", "name"),
               suffix=c("_high", "_low")) %>%
-    left_join(import(low_clamp) %>% select(-rna_log10_padj_high_v_reporter)) %>%
+    left_join(import(low_clamp) %>% rename(rna_log10_padj_low_clamp_v_reporter=rna_log10_padj_high_v_reporter)) %>%
     rename(rna_enrichment_low_clamp = rna_enrichment) %>%
     write_tsv(output)
 
